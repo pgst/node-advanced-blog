85f7d4558a44ed001a23e4b6062c44a8f1b6a9ee
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-03 17:32:10 +0900

    MongoDBにデータを書き込む

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+// Connecting to MongoDB
 mongoose
   .connect(
     'mongodb+srv://pgstaka:Guru0521!@cluster0.gw0z3um.mongodb.net/blogUserDatabase?retryWrites=true&w=majority&appName=Cluster0'
@@ -11,23 +14,45 @@ mongoose
   })
   .catch((err) => console.log('Failure: Unconnected to MongoDB'));
 
+// Defining the Schema and Model
+const Schema = mongoose.Schema;
+const BlogSchema = new Schema({
+  title: String,
+  summary: String,
+  image: String,
+  textBody: String,
+});
+const BlogModel = mongoose.model('blog', BlogSchema);
+
+// BLOG function
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Create blog
 app.get('/blog/create', (req, res) => {
   res.sendFile(__dirname + '/views/blogCreate.html');
 });
 
 app.post('/blog/create', (req, res) => {
   console.log('reqの中身', req.body);
-  res.send('ブログデータを投稿しました');
+  BlogModel.create(req.body, (error, savedBlogData) => {
+    if (error) {
+      console.log('データの書き込みが失敗しました');
+      res.send('ブログデータの投稿が失敗しました');
+    } else {
+      console.log('データの書き込みが成功しました');
+      res.send('ブログデータの投稿が成功しました');
+    }
+  });
 });
 
-// app.post('/', (req, res) => {
-//   console.log('POST request received');
-// });
+// Read All Blogs
+// Read Single Blog
+// Update Blog
+// Delete Blog
 
+// Connecting to port
 app.listen(5000, () => {
   console.log('Listening on localhost port 5000');
 });
