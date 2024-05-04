4b98c2e8ad3aa64415ad974a403fbc8c73f625d2
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-04 15:12:29 +0900

    MongoDBのデータを削除する

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ const BlogSchema = new Schema({
 const BlogModel = mongoose.model('blog', BlogSchema);
 
 // BLOG function
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 // Create blog
 app.get('/blog/create', (req, res) => {
   res.sendFile(__dirname + '/views/blogCreate.html');
@@ -48,9 +44,56 @@ app.post('/blog/create', (req, res) => {
 });
 
 // Read All Blogs
+app.get('/', async (req, res) => {
+  const allBlogs = await BlogModel.find();
+  console.log('全ブログデータ', allBlogs);
+  res.send('全ブログデータを読み取りました');
+});
+
 // Read Single Blog
+app.get('/blog/:id', async (req, res) => {
+  const singleBlog = await BlogModel.findById(req.params.id);
+  console.log('singleBlog', singleBlog);
+  res.send('個別の記事ページ');
+});
+
 // Update Blog
+app.get('/blog/update/:id', async (req, res) => {
+  const singleBlog = await BlogModel.findById(req.params.id);
+  console.log('singleBlog', singleBlog);
+  res.send('個別の更新ページ');
+});
+
+app.post('/blog/update/:id', async (req, res) => {
+  BlogModel.updateOne({ _id: req.params.id }, req.body).exec((error) => {
+    if (error) {
+      console.log('データの更新が失敗しました');
+      res.send('ブログデータの更新が失敗しました');
+    } else {
+      console.log('データの更新が成功しました');
+      res.send('ブログデータの更新が成功しました');
+    }
+  });
+});
+
 // Delete Blog
+app.get('/blog/delete/:id', async (req, res) => {
+  const singleBlog = await BlogModel.findById(req.params.id);
+  console.log('singleBlog', singleBlog);
+  res.send('個別の削除ページ');
+});
+
+app.post('/blog/delete/:id', async (req, res) => {
+  BlogModel.deleteOne({ _id: req.params.id }).exec((error) => {
+    if (error) {
+      console.log('データの削除が失敗しました');
+      res.send('ブログデータの削除が失敗しました');
+    } else {
+      console.log('データの削除が成功しました');
+      res.send('ブログデータの削除が成功しました');
+    }
+  });
+});
 
 // Connecting to port
 app.listen(5000, () => {
