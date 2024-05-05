af9f779cded179e222d006105bac3a8a3c83ddf1
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-05-05 11:24:57 +0900

    Renderへのデプロイ

--- index.js
+++ index.js
@@ -158,6 +158,8 @@ app.post('/user/login', (req, res) => {
 });
 
 // Connecting to port
-app.listen(5000, () => {
-  console.log('Listening on localhost port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
